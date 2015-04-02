70b83333fc07fb103674646199c456cd2e07d7a8
tatiz <ivonne.amaya2@gmail.com>
2015-04-01 19:44:51 -0500

    jmenuBar

--- Formales/src/gui/VentanaPrincipal.java
+++ Formales/src/gui/VentanaPrincipal.java
@@ -1,9 +1,10 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
 public class VentanaPrincipal extends JFrame{
-
+	private JMenuBar jmenu;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
