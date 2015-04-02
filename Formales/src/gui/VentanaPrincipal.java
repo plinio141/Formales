1ec12bfd9675844c3b7d45277c61f12ad3f0fc34
tatiz <ivonne.amaya2@gmail.com>
2015-04-01 20:50:26 -0500

    V1

--- Formales/src/gui/VentanaPrincipal.java
+++ Formales/src/gui/VentanaPrincipal.java
@@ -1,10 +1,46 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class VentanaPrincipal extends JFrame{
-	private JMenuBar jmenu;
+	/* 
+	 * barra de menú
+	 */
+	private JMenuBar menu;
+	/*
+	 * Menu opciones
+	 */
+	private JMenu file;
+	/*
+	 * item que permite abrir archivo
+	 */
+	private JMenu automata;
+	/*
+	 * crear un AFD, AFND
+	 */
+	private JMenu newA;
+	/*
+	 * ejecutar el automata
+	 */
+	private JMenu ejecutar;
+	/*
+	 * item que permite abrir un automata
+	 */
+	private JMenuItem open;
+	/*
+	 * item que permite guardar archivo
+	 */
+	private JMenuItem save;
+	/*
+	 * 
+	 */
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
